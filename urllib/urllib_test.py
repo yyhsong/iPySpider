d911dfbde0ce14e858c12db937614e5100a506f3
Neo <hwhsong@163.com>
2018-11-29 20:07:46 +0800

    urllib库基本使用

--- urllib/urllib_test.py
+++ urllib/urllib_test.py
@@ -19,11 +19,11 @@ readline(): 读取一行内容
 from urllib import request
 import time
 
-url = 'https://blog.csdn.net/hwhsong/article/details/80805511'
+url = 'https://blog.csdn.net'
 req = request.Request(url)
 req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36')
 for i in range(100):
-    with request.urlopen(req, timeout=10) as f:
+    with request.urlopen(req, timeout=100) as f:
         print('-------------- Request --------------')
         print('Status:', f.status)
         print('Reason:', f.reason)
