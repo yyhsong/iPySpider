c2a6cdbff46ada84695ecdde993bd7471dd0d2f0
Neo <hwhsong@163.com>
2020-10-27 11:33:09 +0800

    GET请求的基本用法

--- urllib/urllib_get.py
+++ urllib/urllib_get.py
@@ -3,16 +3,23 @@
 
 """urllib
 
+使用urllib发送GET方式的请求
+把请求返回的内容写入本地文件
 """
 
 from urllib import request
 
+# 爬取的目标网址
 url = 'https://www.baidu.com/s?wd='
-# 对中文进行编码
-wd = request.quote('爬虫')
+
+# 设置请求头信息，标识User-Agent
+wd = request.quote('爬虫')  # 对中文进行编码
 url += wd
 req = request.Request(url)
-req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36')
+req.add_header('User-Agent',
+               'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36')
 
-with open('urllib_get_result.html', 'wb') as f:
+# 把爬取内容写入本地文本文件
+with open('urllib_get_result.txt', 'wb') as f:
     f.write(request.urlopen(req).read())
+    print('----- 写入结束 -----')
